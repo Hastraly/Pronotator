d6b7295ff51e99c8b9445fb9eb2875286bc92e77
Hastraly <radix.kelian@gmail.com>
2025-10-04 11:22:10 +0200

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -50,7 +50,13 @@ app.use((req, res, next) => {
   // importantly only setup vite in development and after
   // setting up all the other routes so the catch-all route
   // doesn't interfere with the other routes
-    await setupVite(app, server);
+    if (process.env.NODE_ENV === "development") {
+  await setupVite(app, server);
+} else {
+  console.log("Production mode detected — serving static files");
+  serveStatic(app);
+}
+
 
   // ALWAYS serve the app on the port specified in the environment variable PORT
   // Other ports are firewalled. Default to 5000 if not specified.
